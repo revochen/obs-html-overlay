ed7294422c629301b9b748734dfaf528ea612bd2
revochen <revochen@users.noreply.github.com>
2022-08-14 14:31:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,69 @@
-import { h, render } from "https://unpkg.com/preact@latest?module";
-// In case you need hooks uncomment this line
-// import {} from "https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module";
-import htm from "https://unpkg.com/htm?module";
+let $debug = window.localStorage.getItem('Debug');
 
-// Initialize htm with Preact
-const html = htm.bind(h);
+window.addEventListener('load', (event) => {
+    //console.log('page is fully loaded');
+    //setInterval(UpdateCanvas,100);
+});
 
-render(
-  html`
-    <${Plugin} />
-  `,
-  document.querySelector("#root")
-);
+function debug($dbg) {
+    if ($debug) {
+        console.log("DEBUG " + $dbg);
 
-function Plugin() {
-  const params = new URLSearchParams(window.location.search);
-  const username = params.get("username") || "World";
+    }
+}
 
-  return html`
-    Hello ${username}
-  `;
+function setDebug() {
+    $di = document.getElementById('debugToggle');
+    $debug = $di.checked;
+    window.localStorage.setItem('Debug', $debug);
+    
+    debug("Setting debug to " +$debug);
+}
+
+function setAnimationType() {
+
+    $ai = document.getElementById('aniTypeInput');
+    window.localStorage.setItem('Animation Type', $ai.value);
+
+    debug("update animation type " + $ai.value);
+}
+
+function setColor() {
+    $ci = document.getElementById('colorInput');
+    window.localStorage.setItem('Color', $ci.value);
+
+    debug("update color " + $ci.value);
+}
+
+function setLabelText() {
+
+    debug("Reading text input field");
+
+    $ti = document.getElementById('textInput');
+    window.localStorage.setItem('Text Input', $ti.value);
+}
+
+function resetLocalStorage() {
+    debug('Resetting local storage');
+
+    window.localStorage.clear();
+    $ti = document.getElementById('textInput');
+    $ti.value = "";
+}
+
+function Setup() {
+
+}
+function SetupLabel() {
+    // Setup html div label
+    debug("Setting up label");
+
+    $tl = document.getElementById('textLabel');
+    $label = window.localStorage.getItem('Text Input');
+    $tl.style.color = window.localStorage.getItem('Color');
+    $aniType = window.localStorage.getItem('Animation Type');
+    $tl.className = "animate__animated animate__" + $aniType + " animate__infinite center";
+    //$tl.classList.add("center");
+    $tl.innerHTML = "<h1>"+$label+"</h1>";
+
 }
